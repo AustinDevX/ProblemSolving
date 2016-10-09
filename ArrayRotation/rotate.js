9af0da790ea56362fa2e20dd20a4885f7ad61c43
Kadeem Austin <kadeemaustin195@gmail.com>
2016-10-09 08:07:37 -0400

    Small logic fixes and addtional test

--- ArrayRotation/rotate.js
+++ ArrayRotation/rotate.js
@@ -1,18 +1,25 @@
 const mocha   = require('mocha');
 const assert  = require('chai').assert;
 
-describe(".rotateArray", function(){
-  it("should rotate the array by the specified number of places", function(){
-    testArray = [1, 2, 3, 4, 5];
-    rotateArray(testArray, 2);
-
-    rotatedArray = [3, 4, 5, 1, 2]; // correct array after 2 place rotation
-
-    // for(let i=0; i < testArray.length; i++) {
-    //   assert.strictEqual(testArray[i], rotatedArray[i], "Array element found in incorrect position");
-    // }
+describe("Array Rotation", function() {
+  describe(".rotateArray", function(){
+    it("should rotate the array by the specified number of places", function(){
+      testArray = [1, 2, 3, 4, 5];
+      rotateArray(testArray, 2);
+      rotatedArray = [3, 4, 5, 1, 2]; // correct array after 2 place rotation
+      assert.deepEqual(testArray, rotatedArray);
+    });
   });
-})
+
+  describe(".rotateByOne", function(){
+    it("Should shift each element in the list to the left by one place", function() {
+      testArray = [1, 2, 3];
+      rotateByOne(testArray);
+      rotatedArray = [2, 3, 1];
+      assert.deepEqual(testArray, rotatedArray);
+    });
+  });
+});
 
 /**
  * Performs a circular, in place, rotation of an array by shifting its elements
@@ -23,21 +30,27 @@ describe(".rotateArray", function(){
  *      rotateArray([1, 2, 3, 4, 5], 1);
  *      //=> [2, 3, 4, 5, 1]
  *
- * @param  {Array.<number>} A    The array to be rotated
- * @param  {number}         p      The number of positions to rotate by
+ * @param  {Array.<number>} A     The array to be rotated
+ * @param  {number}         p     The number of positions to rotate by
  */
 function rotateArray(A, p) {
     // rotate the array by one p times
-    for(let i=0; i <= p; i++){
-        rotByOne(A)
+    for(let i=1; i <= p; i++){
+        rotateByOne(A)
     }
+}
 
-  // bubble the first element to the end thereby rotating the array by one
-  function rotByOne(A) {
-    for(let i=0; i < A.length-1; i++) {
-      A[i] = A[i] ^ A[i+1]
-      A[i+1] = A[i] ^ A[i+1]
-      A[i] = A[i] ^ A[i+1]
-    }
+/**
+ * Bubbles the first element to the end of the list thereby performing a single rotation
+ * The array is rotated in place
+ *
+ * @param  {Array.<number>} A     The array to be rotated
+ */
+function rotateByOne(A) {
+  for(let i=0; i < A.length-1; i++) {
+    // bit swap elements
+    A[i] = A[i] ^ A[i+1]
+    A[i+1] = A[i] ^ A[i+1]
+    A[i] = A[i] ^ A[i+1]
   }
 }
